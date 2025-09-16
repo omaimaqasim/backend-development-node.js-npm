2d18505eec931ea1afc14f0c58a7f92dca962b51
omaimaqasim <omaimaakhan15@gmail.com>
2025-09-16 18:36:25 +0500

    mongodb concepts

---
+++ mongoDb/server.js
@@ -0,0 +1,15 @@
+// first download mongodb 
+// database is where your backend and frondend data store all files
+
+// first install mongodb then mongo campass 
+// your all data in mongodb is persist it is store and never be remove until you do
+
+
+// if you want to interact with mongodb then we use mongodb shell
+
+
+// in mongo shell :
+// show databases : it shows all your persist databases
+// use web development : it will switch to web development database
+// db.concepts.find() : it will give all data from that 
+// db.concepts.insertone() : you can add add by this
