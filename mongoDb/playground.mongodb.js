4de16ac1d38fe3ba26adc4e4bcb68e686e310106
omaimaqasim <omaimaakhan15@gmail.com>
2025-09-18 17:54:03 +0500

    concepts of mongodb

---
+++ mongoDb/playground.mongodb.js
@@ -0,0 +1,31 @@
+
+
+// Select the database to use.
+use('webDevelopment');
+
+// Insert a few documents into the sales collection.
+db.getCollection('concepts').insertMany([
+
+  {
+    name: "zeyden",
+    class: "js concepts"
+  },
+  {
+    name: "ali",
+    class: "html basics"
+  },
+  {
+    name: "sara",
+    class: "css styling"
+  },
+  {
+    name: "ahmed",
+    class: "react intro"
+  }
+
+
+]);
+
+// Print a message to the output window.
+console.log(`done inserting data`);
+
