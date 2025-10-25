d032b3d231b875439c8d3222ae190c370af8eeae
omaimaqasim <omaimaakhan15@gmail.com>
2025-10-25 13:42:23 +0500

    collecting concepts

--- introduction_expressjs/server.js
+++ introduction_expressjs/server.js
@@ -5,6 +5,17 @@ const port = 4000
 app.get("/",(req,res)=>{
     res.send("hello ummi ")
 })
+app.get("/about",(req,res)=>{
+    res.send("hello my name is ummi i am beginner programmer")
+})
+app.get("/contact",(req,res)=>{
+    res.send("please contect me on insta ummiProgrammer@22")
+})
+// :slug is like placeholder where what you write in it it will be store to slug
+app.get("/user/:name/:age",(req,res)=>{
+    res.send(`Username : ${req.params.name} , age : ${req.params.age}`)
+})
+
 app.listen(port,()=>{
     console.log(`Server started successfully on port ${port}`)
 })
