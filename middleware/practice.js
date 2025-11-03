77f7d221fcdaabac7307a851ae20eafa533263fa
omaimaqasim <omaimaakhan15@gmail.com>
2025-11-03 19:37:39 +0500

    understanding cncepts

--- middleware/practice.js
+++ middleware/practice.js
@@ -1,6 +1,22 @@
 const express = require('express')
 const app = express()
 
-app.use(express.static('public'))
+
+// middleware exp
+function logger(req,res,next){
+  console.log("request method :",req.method)
+  next()
+}
+
+app.use(logger)
+
+app.get('/', (req, res) => {
+  res.send('Hey fellows! Request method is: ' + req.method)
+
+})
 
 app.listen(5000, () => console.log('Server chal gaya'))
+
+// we use routes for making our code orginaized and prevent it
+// from getting messy . we use middleware if we want to reused a code
+// everytime whenever each route run
