f9ed90a5f58710b233113599283623271f3ed38d
omaimaqasim <omaimaakhan15@gmail.com>
2026-01-07 14:56:23 +0500

    try using navbar in main app

--- react/with-react/src/App.jsx
+++ react/with-react/src/App.jsx
@@ -1,18 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
-
+import navbar from '../src/components/navbar'
 function App() {
   // This line of code uses useState to create a variable called value, a function called setValue to change that variable, and initializes value with 0.
   //usestate fn give two thing one variable and second fn to change it
   const [value,setvalue] = useState(0)
   return (
     <div className="App">
+      <Navbar/>
       <div className="className">
         {value}
         </div> 
         <button onClick={()=>{setvalue(value+1)}}>click me</button>
     
+
     </div>
   );
 }
---
+++ react/with-react/src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+//i just write rafce then it give me react arrow fn of my file
+const navbar = () => {
+  return (
+    <div>
+      <ul>
+        <li>home</li>
+        <li>about</li>
+        <li>contact us</li>
+      </ul>
+    </div>
+  )
+}
+
+export default navbar
