ef1bb6492897f1839443e04397d16be767af9b75
omaimaqasim <omaimaakhan15@gmail.com>
2026-01-11 17:07:08 +0500

    understanding import

--- components,props,jsx_in_react/first-reactapp/src/App.jsx
+++ components,props,jsx_in_react/first-reactapp/src/App.jsx
@@ -1,4 +1,5 @@
 
+import Navbar from "./components/Navbar"
 function App() {
 
 
@@ -6,7 +7,7 @@ function App() {
     //this is just a wraper and this is the syntax to use jsx which means using js with html at exact time
    //remmeber that in return their should be one div or wraper in that you should write code for your app
    <>
-     
+    <Navbar/>
     </>
   )
 }
---
+++ components,props,jsx_in_react/first-reactapp/src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import "../components/Navbar.css"
+function Navbar() {
+  return (
+    <div>
+      <ul>
+        <li>home</li>
+        <li>about us</li>
+        <li>contact us</li>
+      </ul>
+    </div>
+  )
+}
+
+export default Navbar
