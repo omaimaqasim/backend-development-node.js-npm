fdd30a57781b612c27f712d7f55b5097477d2c0d
omaimaqasim <omaimaakhan15@gmail.com>
2026-01-14 17:02:26 +0500

    understanding inline css in jsx

--- components,props,jsx_in_react/first-reactapp/src/components/Card.jsx
+++ components,props,jsx_in_react/first-reactapp/src/components/Card.jsx
@@ -3,8 +3,10 @@ import "../components/Card.css"
 const Card = (props) => {
   return (
     <div>
-      <div className="card">
-
+      {/* in jsx you cannot write inline css with string for that you have to use {{}} outerone for js and inner one is obj that has all css property */}
+      
+      <div className="card" style={{overflow : 'hidden'}}>
+     <img src="pak.jpg" alt="" width={150} />
         <h1>{props.title}</h1>
         <p>{props.description}</p>
       </div>
---
+++ components,props,jsx_in_react/first-reactapp/src/information.md
@@ -0,0 +1,3 @@
+**shortcut for going to specific component from amin app.jsx**
+
+> just go to tag of that component then with ctrl + click you can easily go to that component
