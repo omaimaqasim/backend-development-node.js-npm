85b5801a69d07a9e0ed9f4048fc9eb2b02d6bd1b
omaimaqasim <omaimaakhan15@gmail.com>
2026-01-17 17:00:02 +0500

    understanding useEffect hook

--- hooks and states in react/second-app/information.md
+++ hooks and states in react/second-app/information.md
@@ -7,3 +7,16 @@
 ``
 code -r project name
 ``
+
+
+*** why useEffect showing mount component two time ? ***
+
+``
+becase of   <StrictMode> in main.jsx as you know that in production becuase of this react make that component to work one for check and to work one for real
+``
+
+
+
+
+
+Exactly, that’s a perfect way to put it! Rendering is like the planning or the blueprint phase, where React decides what the UI should look like. Mounting, on the other hand, is when that plan actually becomes reality, and the component appears in the DOM.
--- hooks and states in react/second-app/src/App.jsx
+++ hooks and states in react/second-app/src/App.jsx
@@ -1,10 +1,23 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+
+  //useeffect hook in react: we use this hock for making effect when element or component mounts that me 
+  //when component enter or work in app
+
+  useEffect(() => {
+   alert("page is loaded")
+  }, []) // empty arr is saying that make it work when app is mounting
+
+  useEffect(() => {
+   alert("count is clicked")
+  }, [count]) // make it work when count is mounting 
+
+  
 
   return (
     <>
