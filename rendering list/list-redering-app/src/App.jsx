961fbc1547529cdf9d9c8290b09e6316713b3f26
omaimaqasim <omaimaakhan15@gmail.com>
2026-02-05 15:53:42 +0500

    understanding list and conditional rendering

---
+++ rendering list/list-redering-app/src/App.jsx
@@ -0,0 +1,57 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+  const [showbtn, setshowbtn] = useState(true)
+  const [rendertodo, setrendertodo] = useState([
+
+    {
+      title: "hay i am todo 1",
+      desc : " i am lovely todo 1 complete me"
+    },
+
+    {
+      title: "hay i am todo 2",
+      desc : " i am lovely todo 2 complete me"
+    },
+
+    {
+      title: "hay i am todo 3",
+      desc : " i am lovely todo 3 complete me"
+    }
+  ])
+
+//   The core reason (simple)
+// In React, capital letters tell React: “this is a component”.
+// Lowercase tells React: “this is an HTML tag.”
+//if you want to make a sall conponent you can easily make it in this app fn also for example
+  const Todo = (todo)=>{
+    return (<>
+        <div className="todo">{todo.title}</div>
+        <div className="desc">{todo.desc}</div>
+    </>)
+  }
+
+  return (
+    <>
+     
+      {rendertodo.map(todo=>{
+        return <Todo todo = {todo}/>
+      })}
+      <div className="card">
+        <button onClick={() => setshowbtn(!showbtn)}>
+            showbtn render
+        </button>
+      
+      </div>
+    
+      { showbtn && <button>hay htn is showing</button> }
+       {/* { showbtn ? <button>hay htn is showing</button> : ""} */}
+    </>
+  )
+}
+
+export default App
