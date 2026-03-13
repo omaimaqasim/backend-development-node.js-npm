20146f0cea4b65b350681336706f592bb7aca80b
omaimaqasim <omaimaakhan15@gmail.com>
2026-03-13 06:19:46 +0500

    understand path module

--- topic_related_tasks/task_01_file_node.js/task1.js
+++ topic_related_tasks/task_01_file_node.js/task1.js
@@ -1,39 +1,17 @@
-import fileSystem from "fs";
 import fs from "fs/promises";
 
-// remember this promises does not accept callback so use then
-fileSystem.writeFile("student.txt"," ali \n ahmed \n zara \n zeyd",(err)=>{
-     if(err) throw err;
-    console.log("file successfully created! \n");
+async function main() {
+  // Create students.txt
+  await fs.writeFile("student.txt", "Ali\nAhmed\nSara\nZain\n");
 
-})
+  // Append more students
+  await fs.appendFile("student.txt", "Ayesha\nHamza\n");
 
+  // Append log.txt
+  const now_time = new Date();
+  await fs.appendFile("log.txt", "\nProgram executed at: " + now_time);
 
+  console.log("Log updated successfully!");
+}
 
-
-fileSystem.readFile("student.txt",(e,d)=>{
-    console.log("students : \n"+d.toString())
-})
-
-// appending students append file does ot return data
- await fs.appendFile("student.txt","\n zeyn \n zehra") 
-//    why use await bcause all are async fn so compiler real file before
-// append that why we use await
-
-
-// again read file
-fileSystem.readFile("student.txt",(e,d)=>{
-    console.log(d.toString().split("\n"))
-    console.log(d.toString().split("\n").length)
-})
-
-// append current date with time after every execution
-
-let now_time = new Date();
-
-await fs.appendFile("log.txt", "\nProgram executed at: " + now_time);
-
-console.log("log updated successfully");
-
- 
-
+main().catch(err => console.log(err));
