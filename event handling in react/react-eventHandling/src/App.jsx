1a6882fd757d799b44c96269341484c87304b02a
omaimaqasim <omaimaakhan15@gmail.com>
2026-07-19 18:48:46 +0500

    understand event handling in react

--- event handling in react/react-eventHandling/src/App.jsx
+++ event handling in react/react-eventHandling/src/App.jsx
@@ -8,6 +8,7 @@ function App() {
 const [Text, setText] = useState("");
 const [HoverText, setHoverText] = useState("Hover me");
 const [Input, setInput] = useState("zeyd");
+const [Form, setForm] = useState({email:"" , phone: ""});
 
 
 const handleClick = ()=>{
@@ -23,6 +24,17 @@ const changeValue = (e)=>{
   setInput(e.target.value);
 }
 
+//only using one handle function multiple inputs ko handle karsakte hain
+const handleChange = (e)=>{
+
+  setForm({...Form, [e.target.name]:e.target.value});
+  // key: → Use the word key.
+// ✅ [key]: → Use the value inside key.
+// "JavaScript, look inside this variable and use its value."
+//first sprad form properties key and values then we replae those values with current value
+  console.log(Form)
+}
+
   return (
     <>
      
@@ -32,6 +44,8 @@ const changeValue = (e)=>{
      <div className="hover" onMouseOver={handleHover}>{HoverText}</div>
 
      <input type="text" value={Input} onChange={changeValue} />
+     <input type="text " name='email' value={Form.email} onChange={handleChange} />
+     <input type="text" name='phone' value={Form.phone} onChange={handleChange} />
      {/* why need onchange event because without it whatever you write js dont now we use this event for storing
      value so if we need it js knows */}
      
