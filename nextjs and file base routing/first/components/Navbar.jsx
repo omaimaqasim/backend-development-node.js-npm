7c496c692e8fd60eb76125d4889ef8eb5fda69da
omaimaqasim <omaimaakhan15@gmail.com>
2026-08-12 16:41:04 +0500

    understanding foundation of nextjs

---
+++ nextjs and file base routing/first/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+       <nav>
+        <ul className='flex gap-3.5 px-3.5'>
+            <li>home</li>
+            <li>about</li>
+            <li>contact</li>
+            <li>info</li>
+        </ul>
+       </nav>
+    </div>
+  )
+}
+
+export default Navbar
---
+++ nextjs and file base routing/info.md
@@ -0,0 +1,1 @@
+npx create-next-app@latest // run command for installing nextjs with reacty
